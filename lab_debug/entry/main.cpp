1d4690b80e2bd2d33750a291cb1a827b729c02e4
ZheyuZhou <zheyuzhou99@gmail.com>
2023-01-08 20:24:01 -0500

    debug

--- lab_debug/entry/main.cpp
+++ lab_debug/entry/main.cpp
@@ -1,5 +1,7 @@
 #include "sketchify.h"
 
+#include <iostream>
+
 /**
  * A simple wrapper function that invokes sketchify on files in the current
  * directory. Input is read from`in.png` and the resulting image is written
@@ -9,6 +11,7 @@ int main() {
     // If you get PNG Decoder error, it is because there is no such PNG "in.png".
     // Kindly change the input to one of the inputs you want to test in order to
     // make it work (ie. in_01.png)
+    std::cout << "Start sketchify" << std::endl;
     sketchify("../tests/in.png", "../tests/out.png");
     return 0;
 }
--- lab_debug/src/sketchify.cpp
+++ lab_debug/src/sketchify.cpp
@@ -29,7 +29,7 @@ PNG* setupOutput(unsigned w, unsigned h) {
  * @return a pointer to the color to use when sketchifying
  */
 HSLAPixel* myFavoriteColor() {
-    HSLAPixel* p = new HSLAPixel(-1, 0.8, 0.5);
+    HSLAPixel* p = new HSLAPixel(113, 0.8, 0.5);
     return p;
 }
 
@@ -44,41 +44,41 @@ void sketchify(std::string inputFile, std::string outputFile) {
     // Create out.png
     // PNG* output = new PNG();
     PNG* output = setupOutput(width, height);
-    std::cout << "output width " << output->width() << std::endl;
-    std::cout << "output height " << output->height() << std::endl;
+    // std::cout << "output width " << output->width() << std::endl;
+    // std::cout << "output height " << output->height() << std::endl;
     // Load our favorite color to color the outline
     HSLAPixel* myPixel = myFavoriteColor();
     // Go over the whole image, and if a pixel differs from that to its upper
     // left, color it my favorite color in the output
     for (unsigned y = 1; y < height; y++) {
         for (unsigned x = 1; x < width; x++) {
-            // std::cout << "Reached line " << __LINE__ << std::endl;
+            // std::cout << "In For loop " << __LINE__ << std::endl;
             // Calculate the pixel difference
             HSLAPixel& prev = original->getPixel(x - 1, y - 1);
-            std::cout << "Reached line " << prev << std::endl;
-            return;
+            // std::cout << "Got Prev Pixel " << prev << std::endl;
             HSLAPixel& curr = original->getPixel(x, y);
-            // std::cout << "Reached line " << __LINE__ << std::endl;
+
             double diff = std::fabs(curr.h - prev.h);
+            // std::cout << "diff " << diff << "_"<< curr.h << "_" << prev.h << std::endl;
             // If the pixel is an edge pixel,
             // color the output pixel with my favorite color
-            // std::cout << "Reached line " << __LINE__ << std::endl;
-            // std::cout << "(*output).getPixel(x, y) " << (*output).getPixel(x, y) << std::endl;
+
             HSLAPixel currOutPixel = (*output).getPixel(x, y);
-            // std::cout << "Reached line " << __LINE__ << std::endl;
+            // std::cout << "currOutPixel x y " << currOutPixel <<std::endl;
             if (diff > 20) {
-                // std::cout << "Reached line " << __LINE__ << std::endl;
+                // std::cout << "in diff " << std::endl;
                 currOutPixel = *myPixel;
-                // std::cout << "Reached line " << __LINE__ << std::endl;
+                // std::cout << "get currOutPixel " << __LINE__ << std::endl;
             }
         }
     }
-    std::cout << "Reached line " << __LINE__ << std::endl;
+    std::cout << "Out of For Loop " << __LINE__ << std::endl;
 
 
     // Save the output file
+    std::cout << "output " << *output << std::endl;
     output->writeToFile(outputFile);
-
+    std::cout <<"Reached line " << __LINE__ << std::endl;
     // Clean up memory
     delete myPixel;
     delete output;
